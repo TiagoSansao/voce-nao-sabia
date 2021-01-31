7d0b59a5f0fe403f7a8c7b143da228bc4288e755
tiago <tiagossansao@gmail.com>
2021-01-30 23:58:19 -0300

    Started building landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width initial-scale=1.0" />
         <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
-        Opaaa
+        <section className={styles.mainTop}>
+          <div className={styles.mainLeft}>
+            <h2 className={styles.mainTextH2}>
+              Aprendizado e<br />Entreitenimento
+            </h2>
+            <h3 className={styles.mainTextH3}>
+              Você encontra aqui
+            </h3>
+            <h4 className={styles.mainTextH4}>
+              Divirta-se com curiosidades interessantísimas que<b> Você não Sabia</b>
+            </h4>
+          </div>
+          <img src="/preta.png" alt="Dog Image" className={styles.mainImage} />
+        </section>
       </main>
 
       <footer className={styles.footer}>
