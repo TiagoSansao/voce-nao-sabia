bf0175dda042103872da6264d41cf9ba953845ed
tiago <tiagossansao@gmail.com>
2021-02-04 17:52:08 -0300

    Created manifest.json and added meta tags in order to get better SEO

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
-        <Head> 
+        <Head>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3317b7" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
           <script data-ad-client="ca-pub-8981365857371901" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
